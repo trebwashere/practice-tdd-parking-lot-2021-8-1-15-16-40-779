d6e065649ce0cdde6ef08d271a8fc7d367301b5b
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-06 20:05:07 +0800

    [STParkCar][Robert] fix: Removed unnecessary requirement and wrong implementation for adding Customer class

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -41,11 +41,9 @@ public class ParkingLotTest {
 
     @Test
     public void should_return_car_when_fetch_given_a_parking_lot_with_parked_car_and_a_parking_ticket() {
-        Customer customer = new Customer();
         Car car = new Car();
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
-        customer.addParkingTicket(parkingTicket);
-        Car returnedCar = parkingLot.fetch(customer,parkingTicket);
+        Car returnedCar = parkingLot.fetch(parkingTicket);
 
         assertEquals(returnedCar, car);
     }
@@ -61,50 +59,27 @@ public class ParkingLotTest {
 
     @Test
     public void should_not_fetch_car_given_a_parking_lot_with_parked_car_but_invalid_ticket() {
-        Customer customer = new Customer();
         Car car = new Car();
         parkingLot.parkCar(car);
         ParkingTicket fakeParkingTicket = new ParkingTicket();
-        Car outputCar = parkingLot.fetch(customer,fakeParkingTicket);
+        Car outputCar = parkingLot.fetch(fakeParkingTicket);
 
         assertNull(outputCar);
     }
 
     @Test
     public void should_not_be_able_to_fetch_car_given_a_parking_lot_with_parked_car_but_reused_ticket() {
-        Customer customer = new Customer();
         Car car = new Car();
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
-        customer.addParkingTicket(parkingTicket);
-        parkingLot.fetch(customer,parkingTicket);
-        Car reusedTicketForCar = parkingLot.fetch(customer,parkingTicket);
+        parkingLot.fetch(parkingTicket);
+        Car reusedTicketForCar = parkingLot.fetch(parkingTicket);
         assertNull(reusedTicketForCar);
     }
 
     @Test
-    public void should_not_be_able_to_fetch_car_of_another_customer_if_ticket_not_owned_by_fetching_customer_was_used() {
-        Customer bert = new Customer();
-        Customer david = new Customer();
-
-        Car bertCar = new Car();
-        Car davidCar = new Car();
-
-        ParkingTicket bertParkingTicket = parkingLot.parkCar(bertCar);
-        bert.addParkingTicket(bertParkingTicket);
-        ParkingTicket davidParkingTicket = parkingLot.parkCar(davidCar);
-        david.addParkingTicket(davidParkingTicket);
-
-        Car bertWithDavidTicket = parkingLot.fetch(bert, davidParkingTicket);
-        Car davidWithBertTicket = parkingLot.fetch(david, bertParkingTicket);
-
-        assertNull(bertWithDavidTicket);
-        assertNull(davidWithBertTicket);
-    }
-
-    @Test
     public void should_display_Unrecognized_parking_ticket_when_car_is_fetched_but_no_car_was_released() {
-        should_not_fetch_car_given_a_parking_lot_with_parked_car_but_invalid_ticket();
-        assertTrue(systemOut().contains("Unrecognized parking ticket."));
+        Exception exception = assertThrows(RuntimeException.class, this::should_not_fetch_car_given_a_parking_lot_with_parked_car_but_invalid_ticket);
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
     }
 
     @Test
