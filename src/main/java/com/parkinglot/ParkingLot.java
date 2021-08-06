26e297ef411b6b890cbbe0449b5438b8d38798ff
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-06 13:54:43 +0800

    [STParkCar][Robert] refactor: Changed ParkingLot to a singleton

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -3,6 +3,20 @@ package com.parkinglot;
 public class ParkingLot {
     private Car car;
     private ParkingTicket parkingTicket;
+    private static ParkingLot parkingLot;
+
+    private ParkingLot()
+    {
+
+    }
+
+    public static ParkingLot getInstance() {
+        if (parkingLot == null) {
+            parkingLot = new ParkingLot();
+        }
+        return parkingLot;
+    }
+
     public ParkingTicket parkCar(Car car) {
         this.car = car;
         this.parkingTicket = new ParkingTicket();
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -1,12 +1,19 @@
 package com.parkinglot;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
+
+    private ParkingLot parkingLot;
+
+    @BeforeEach
+    public void setup() {
+        parkingLot = ParkingLot.getInstance();
+    }
     @Test
     public void should_return_parking_ticket_given_a_parking_lot_and_a_car() {
-        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
 
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
@@ -15,7 +22,6 @@ public class ParkingLotTest {
 
     @Test
     public void should_return_car_when_fetch_given_a_parking_lot_with_parked_car_and_a_parking_ticket() {
-        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
         ParkingTicket parkingTicket = parkingLot.parkCar(car);
         Car returnedCar = parkingLot.fetch(parkingTicket);
