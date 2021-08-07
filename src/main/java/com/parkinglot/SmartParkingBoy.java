ce266aa5253feae4f843e13badba2ace80a578a4
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-07 13:41:52 +0800

    [STParkCar][Robert] testAndFeat: Added test for SmartParkingBoy to perform all parking boy duties, with the addition of determining parking lots with the most slots

---
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,32 @@
+package com.parkinglot;
+
+import java.util.List;
+import java.util.Optional;
+
+public class SmartParkingBoy extends ParkingBoy{
+
+    public SmartParkingBoy(ParkingLot parkingLot) {
+        super(parkingLot);
+    }
+
+    public SmartParkingBoy(List<ParkingLot> parkingLotList) {
+        super(parkingLotList);
+    }
+
+    public ParkingTicket park(Car car) {
+        return findParkingLotWithMostSlots()
+                .orElseThrow(NoAvailableParkingPositionException::new)
+                .parkCar(car);
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return findParkingLotRelatedToTicket(parkingTicket)
+                .fetch(parkingTicket);
+    }
+
+    private Optional<ParkingLot> findParkingLotWithMostSlots() {
+        return getParkingLots().stream()
+                .filter(parkingLot -> !parkingLot.isFull())
+                .reduce((firstLot, secondLot) -> firstLot.getRemainingParkingLotSlots() >= secondLot.getRemainingParkingLotSlots() ? firstLot : secondLot);
+    }
+}
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -18,8 +18,10 @@ public class ParkingLotTest {
     private Car car;
     private ParkingLot parkingLot;
     private StandardParkingBoy standardParkingBoy;
+    private SmartParkingBoy smartParkingBoy;
     private List<ParkingLot> parkingLotList;
     private StandardParkingBoy standardParkingBoyWithParkingLotList;
+    private SmartParkingBoy smartParkingBoyWithParkingLotList;
 
 
     @BeforeEach
@@ -27,8 +29,10 @@ public class ParkingLotTest {
         car = new Car();
         parkingLot = new ParkingLot();
         standardParkingBoy = new StandardParkingBoy(parkingLot);
+        smartParkingBoy = new SmartParkingBoy(parkingLot);
         parkingLotList = Arrays.asList(parkingLot, parkingLot, parkingLot);
         standardParkingBoyWithParkingLotList = new StandardParkingBoy(parkingLotList);
+        smartParkingBoyWithParkingLotList = new SmartParkingBoy(parkingLotList);
     }
 
     private void throwNoAvailableParkingPositionException() { throw new NoAvailableParkingPositionException(); }
@@ -136,7 +140,7 @@ public class ParkingLotTest {
     @Test
     public void standardParkingBoy_should_return_car_when_fetch_given_a_list_of_parking_lots_with_parked_car_and_a_parking_ticket() {
         ParkingTicket parkingTicket = standardParkingBoyWithParkingLotList.park(car);
-        Car returnedCar = standardParkingBoy.fetch(parkingTicket);
+        Car returnedCar = standardParkingBoyWithParkingLotList.fetch(parkingTicket);
 
         assertEquals(returnedCar, car);
     }
@@ -177,6 +181,62 @@ public class ParkingLotTest {
         ParkingTicket parkingTicket = mockStandardParkingBoy.park(car);
         assertNotNull(parkingTicket);
         assertEquals(mockStandardParkingBoy.getParkingLots().get(1).fetch(parkingTicket), car);
+    }
+
+    @Test
+    public void smartParkingBoy_should_return_parking_ticket_given_a_list_of_parking_lots_and_a_car() {
+        ParkingTicket parkingTicket = smartParkingBoyWithParkingLotList.park(car);
+        assertNotNull(parkingTicket);
+    }
+
+    @Test
+    public void smartParkingBoy_should_return_car_when_fetch_given_a_list_of_parking_lots_with_parked_car_and_a_parking_ticket() {
+        ParkingTicket parkingTicket = smartParkingBoyWithParkingLotList.park(car);
+        Car returnedCar = smartParkingBoyWithParkingLotList.fetch(parkingTicket);
+
+        assertEquals(returnedCar, car);
+    }
+
+    @Test
+    public void smartParkingBoy_should_not_fetch_car_given_a_list_of_parking_lots_with_parked_car_but_invalid_ticket() {
+        smartParkingBoyWithParkingLotList.park(car);
+        ParkingTicket fakeParkingTicket = new ParkingTicket();
+        Exception exception = assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoyWithParkingLotList.fetch(fakeParkingTicket));
+
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    public void smartParkingBoy_should_not_be_able_to_fetch_car_given_a_list_of_parking_lots_with_parked_car_but_reused_ticket() {
+        ParkingTicket parkingTicket = smartParkingBoyWithParkingLotList.park(car);
+        smartParkingBoyWithParkingLotList.fetch(parkingTicket);
+        Exception exception = assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoyWithParkingLotList.fetch(parkingTicket));
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    public void smartParkingBoy_should_not_allow_car_to_park_if_a_list_of_parking_lots_is_full() {
+        ParkingLot spyParkingLot = Mockito.spy(ParkingLot.class);
+        SmartParkingBoy parkingBoyToBeSpied = new SmartParkingBoy(Arrays.asList(spyParkingLot, spyParkingLot, spyParkingLot));
+        SmartParkingBoy mockSmartParkingBoy = Mockito.spy(parkingBoyToBeSpied);
+        Mockito.lenient().when(spyParkingLot.isFull()).thenReturn(true);
+        Exception exception = assertThrows(NoAvailableParkingPositionException.class, () -> mockSmartParkingBoy.park(car));
+        assertEquals("No available position.", exception.getMessage());
+    }
+
+    @Test
+    public void smartParkingBoy_should_park_car_at_parking_lot_with_most_slots_given_a_list_of_parking_lots() {
+        ParkingLot firstSpyParkingLot = Mockito.spy(ParkingLot.class);
+        ParkingLot secondSpyParkingLot = Mockito.spy(ParkingLot.class);
+        ParkingLot thirdSpyParkingLot = Mockito.spy(ParkingLot.class);
+        SmartParkingBoy parkingBoyToBeSpied = new SmartParkingBoy(Arrays.asList(firstSpyParkingLot, secondSpyParkingLot, thirdSpyParkingLot));
+        SmartParkingBoy mockSmartParkingBoy = Mockito.spy(parkingBoyToBeSpied);
+        Mockito.lenient().when(firstSpyParkingLot.getRemainingParkingLotSlots()).thenReturn(0);
+        Mockito.lenient().when(secondSpyParkingLot.getRemainingParkingLotSlots()).thenReturn(10);
+        Mockito.lenient().when(thirdSpyParkingLot.getRemainingParkingLotSlots()).thenReturn(5);
+        ParkingTicket parkingTicket = mockSmartParkingBoy.park(car);
+        assertNotNull(parkingTicket);
+        assertEquals(mockSmartParkingBoy.getParkingLots().get(1).fetch(parkingTicket), car);
     }
 
 }
